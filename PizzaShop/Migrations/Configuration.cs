b0c778e4d6da29cf0b3fd0615fb95df79671eea2
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-12-10 20:36:02 +0200

    Seed user roles & admin account

--- PizzaShop/Migrations/Configuration.cs
+++ PizzaShop/Migrations/Configuration.cs
@@ -5,6 +5,8 @@ namespace PizzaShop.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using PizzaShop.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
 
     internal sealed class Configuration : DbMigrationsConfiguration<PizzaShop.Models.ApplicationDbContext>
     {
@@ -27,6 +29,41 @@ namespace PizzaShop.Migrations
                     PriceCents = 150
                 }
                 );
+
+            // Create user roles
+            var store = new RoleStore<IdentityRole>(context);
+            var manager = new RoleManager<IdentityRole>(store);
+
+            if (!context.Roles.Any(r => r.Name == "admin"))
+            {
+                var role = new IdentityRole { Name = "admin" };
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(r => r.Name == "customer"))
+            {
+                var role = new IdentityRole { Name = "customer" };
+                manager.Create(role);
+            }
+
+            // Create admin user
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var PasswordHash = new PasswordHasher();
+            if (!context.Users.Any(u => u.UserName == "admin@admin.com"))
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "admin@admin.com",
+                    Email = "admin@admin.com",
+                    PhoneNumber = "0401112222",
+                    HomeAddress = "Admin street 1",
+                    HomePostCode = "00001",
+                    HomeCity = "Adminville",
+                    PasswordHash = PasswordHash.HashPassword("Ding123?")
+                };
+
+                UserManager.Create(user);
+                UserManager.AddToRole(user.Id, "admin");
+            }
         }
     }
 }
