21b2d2a823a1de62f3a4e6e446e911845b824500
petrior <petri.raut@gmail.com>
2015-12-10 21:43:46 +0200

    Add PizzaTopping model

---
+++ PizzaShop/Models/PizzaTopping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaShop.Models
+{
+    public class PizzaTopping
+    {
+        public int ID { get; set; }
+        public int PizzaID { get; set; }
+        public int ToppingID { get; set; }
+    }
+}
