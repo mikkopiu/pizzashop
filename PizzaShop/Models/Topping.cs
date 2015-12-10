922bbe8c89adbec2a19c0bc2cae0b50bf89b2d0d
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-12-10 18:03:14 +0200

    Create Topping model

---
+++ PizzaShop/Models/Topping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaShop.Models
+{
+    public class Topping
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int PriceCents { get; set; }
+    }
+}
