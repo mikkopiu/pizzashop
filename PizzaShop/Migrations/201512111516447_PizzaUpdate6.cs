dc8f406e57f84148a5d21300b41bf6af878a109e
petrior <petri.raut@gmail.com>
2015-12-11 17:19:16 +0200

    Updated Pizza name to NOT nullable

---
+++ PizzaShop/Migrations/201512111516447_PizzaUpdate6.cs
@@ -0,0 +1,18 @@
+namespace PizzaShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PizzaUpdate6 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Pizzas", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Pizzas", "Name", c => c.String());
+        }
+    }
+}
