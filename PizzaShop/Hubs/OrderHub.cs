7d2374f8724961de1d42e7e8267ebd0c270ae2c6
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-12-13 17:46:29 +0200

    Setup web sockets for new orders

---
+++ PizzaShop/Hubs/OrderHub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using PizzaShop.Models;
+
+namespace PizzaShop.Hubs
+{
+    public class OrderHub : Hub
+    {
+    }
+}
