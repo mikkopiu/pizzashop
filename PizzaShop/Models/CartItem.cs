dc97d4880c13ede670df0b7f3c63f3f942024691
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-12-13 13:06:11 +0200

    Add ExtraToppings to CartItem

--- PizzaShop/Models/CartItem.cs
+++ PizzaShop/Models/CartItem.cs
@@ -10,6 +10,31 @@ namespace PizzaShop.Models
     {
         // ID = Pizza's ID + timestamp when it was added to the cart
         public long ID { get; set; }
+
+        // Base pizza (includes some un-editable toppings + base price)
         public Pizza Pizza { get; set; }
+
+        // Any extra toppings added from the customer's edit-modal (added to price)
+        public List<Topping> ExtraToppings { get; set; }
+
+        public decimal GetActualPrice()
+        {
+            return GetActualPrice(this);
+        }
+
+        // Get the actual price, i.e. Pizza's base price + any extra Toppings
+        public static decimal GetActualPrice(CartItem item)
+        {
+            decimal price = 0;
+
+            price += item?.Pizza?.PriceEur ?? 0;
+
+            foreach (Topping t in item?.ExtraToppings ?? new List<Topping>())
+            {
+                price += t.PriceEur;
+            }
+
+            return price;
+        }
     }
 }
